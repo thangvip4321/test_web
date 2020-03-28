90845201021f611f2bcd59abc94d76dc7695e987
thangvip4321 <thangvip5432@gmail.com>
2020-03-29 00:58:28 +0700

    hot fix incompatibility

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@ a1=$(pwd)
 loc=/bin/python
 venv_loc=$a1$loc
 python_loc=$(which python)
+echo $venv_loc
 echo $python_loc
+PYTHONPATH=venv/lib/python3.6/site-packages:$PYTHONPATH
+PYTHONPATH=venv/bin:$PYTHONPATH
+export PYTHONPATH
 if [ $venv_loc != $python_loc ]
 then
 	. venv/bin/activate
