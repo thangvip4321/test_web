281aee9c7970d36ffe2441156bffc3e95bec0435
thangvip4321 <thangvip5432@gmail.com>
2020-03-29 13:02:31 +0700

    now make users do the installation themselves

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-instruction:
-sh setup.sh
+#instruction:
+        sh setup.sh  ( to create virtual environment for our packages)
+        export FLASK_APP=testx.py
+        flask run
+
 and open 127.0.0.1:5000 on your browser
+
+##work for python3, not python2.
+###So if you encounter any import error, you may want to change your default interpreter to python3.
+
--- setup.sh
+++ setup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-a1=$(pwd)
-loc=/bin/python
-venv_loc=$a1$loc
-python_loc=$(which python)
-echo $venv_loc
-echo $python_loc
-PYTHONPATH=venv/lib/python3.6/site-packages:$PYTHONPATH
-PYTHONPATH=venv/bin:$PYTHONPATH
-export PYTHONPATH
-if [ $venv_loc != $python_loc ]
-then
-	. venv/bin/activate
-	export FLASK_APP=testx.py
-fi
-flask run
+python3 -m venv venv
+. venv/bin/activate
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
+
+
